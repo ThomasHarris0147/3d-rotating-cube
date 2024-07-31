30911d804c806c17f37bc0ce879bd2c261813bda
thegang_zThomasH <zthomash@pttep.com>
2024-07-31 17:32:19 +0700

    Initial Commit

---
+++ src/views/SphereObject.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { useLoop } from '@tresjs/core'
+import { shallowRef, ref } from 'vue'
+const { onBeforeRender } = useLoop()
+const triangleRef = shallowRef()
+onBeforeRender(({ delta, elapsed }) => {
+  if (triangleRef.value) {
+    triangleRef.value.rotation.y += delta
+  }
+})
+</script>
+<template>
+  <TresMesh cast-shadow ref="triangleRef" :position="[3, -1, 0]">
+    <TresSphereGeometry />
+    <TresMeshToonMaterial color="#FBB03B" />
+  </TresMesh>
+</template>
